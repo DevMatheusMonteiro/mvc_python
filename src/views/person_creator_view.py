c0c051f3096fb199755d93dae436b310733f06e5
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2026-01-13 13:09:53 -0300

    feature: implementing person_creator_view

---
+++ src/views/person_creator_view.py
@@ -0,0 +1,13 @@
+from .interfaces.view_interface import IView
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from src.controllers.interfaces.person_creator_controller_interface import IPersonCreatorController
+
+class PersonCreatorView(IView):
+    def __init__(self, controller: IPersonCreatorController):
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        person_info = http_request.body
+        created_person = self.__controller.create(person_info)
+        return HttpResponse(status_code=201, body=created_person)
